5755c703cd5bb1be00ace162c6aa6ff3c90af148
Lukas Danin <lukas.danin@gmail.com>
2019-08-18 12:31:07 -0300

    Implements Order.arrangements

--- src/order.ts
+++ src/order.ts
@@ -1,3 +1,5 @@
+import Block from "./block";
+import { book } from ".";
 
 export default class Order {
   readonly book1: number = 0;
@@ -24,5 +26,101 @@ export default class Order {
     this.book3 = normalizeAmount(arg.book3);
     this.book4 = normalizeAmount(arg.book4);
     this.book5 = normalizeAmount(arg.book5);
+  }
+
+  /**
+   * Returns all possible ways `Block`s of books can be arranged using the
+   * source of this order.
+   */
+  arrangements(): Block[] {
+    const books: book[] = [];
+    if (this.book1 > 0) books.push("book1");
+    if (this.book2 > 0) books.push("book2");
+    if (this.book3 > 0) books.push("book3");
+    if (this.book4 > 0) books.push("book4");
+    if (this.book5 > 0) books.push("book5");
+
+    if (books.length < 1) return []; // No need to do anything if length < 1
+
+    if (books.length === 1) {
+      return [new Block(...books)];
+    }
+
+    const solo: Block[] = [];
+    const pairs: Block[] = [];
+    const trios: Block[] = [];
+    const quartets: Block[] = [];
+    const quintets: Block[] = [];
+
+    if (books.length === 5) {
+      quintets.push(new Block(...books));
+    }
+
+    const pushToSolo = (b1: book) => {
+      const arrangement = new Block(b1);
+      if (solo.findIndex((a) => a.id === arrangement.id) < 0) {
+        solo.push(arrangement);
+      }
+    };
+
+    const pushToPair = (b1: book, b2: book) => {
+      const arrangement = new Block(b1, b2);
+      if (pairs.findIndex((a) => a.id === arrangement.id) < 0) {
+        pairs.push(arrangement);
+      }
+    };
+
+    const pushToTrio = (b1: book, b2: book, b3: book) => {
+      const arrangement = new Block(b1, b2, b3);
+      if (trios.findIndex((a) => a.id === arrangement.id) < 0) {
+        trios.push(arrangement);
+      }
+    };
+
+    const pushToQuartet = (b1: book, b2: book, b3: book, b4: book) => {
+      const arrangement = new Block(b1, b2, b3, b4);
+      if (quartets.findIndex((a) => a.id === arrangement.id) < 0) {
+        quartets.push(arrangement);
+      }
+    };
+
+    // The easiest way to acquire all possible arrangements these books
+    // can have is by looping through the source and manually find
+    // all possible arragements.
+    const noRepeatedBook = (...check: book[]): boolean => {
+      return (new Set(check).size === check.length);
+    };
+    for (const loop1 of books) {
+      pushToSolo(loop1);
+      for (const loop2 of books) {
+        if (noRepeatedBook(loop1, loop2)) {
+          pushToPair(loop1, loop2);
+
+          // Calculate trios
+          for (const loop3 of books) {
+            // We don't need to run this loop if the amount of books is
+            // less than 3
+            if (books.length < 3) break;
+
+            if (noRepeatedBook(loop1, loop2, loop3)) {
+              pushToTrio(loop1, loop2, loop3);
+
+              // Calculate Quartets
+              for (const loop4 of books) {
+                // We don't need to run this loop if the amount of books
+                // is less than 4
+                if (books.length < 4) break;
+
+                if (noRepeatedBook(loop1, loop2, loop3, loop4)) {
+                  pushToQuartet(loop1, loop2, loop3, loop4);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return [...quintets, ...quartets, ...trios, ...pairs, ...solo];
   }
 }
