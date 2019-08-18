1e00c7b5421750b186ae6f32ce63b9c5f0bb975c
Lukas Danin <lukas.danin@gmail.com>
2019-08-18 15:45:27 -0300

    Allows Order to be constructed with arrays as well

--- src/order.ts
+++ src/order.ts
@@ -1,5 +1,5 @@
-import Block from "./block";
 import { book } from ".";
+import Block from "./block";
 
 export default class Order {
   readonly book1: number = 0;
@@ -8,24 +8,33 @@ export default class Order {
   readonly book4: number = 0;
   readonly book5: number = 0;
 
-  constructor(arg: {
-    book1?: number;
-    book2?: number;
-    book3?: number;
-    book4?: number;
-    book5?: number;
-  }) {
+  constructor(
+    arg: {
+      book1?: number;
+      book2?: number;
+      book3?: number;
+      book4?: number;
+      book5?: number;
+    },
+    argArray?: book[],
+  ) {
     // Orders cannot have books of negative amount, neither of value undefined
     const normalizeAmount = (n?: number): number => {
       if (n === undefined) return 0;
       return n > 0 ? n : 0;
     };
 
-    this.book1 = normalizeAmount(arg.book1);
-    this.book2 = normalizeAmount(arg.book2);
-    this.book3 = normalizeAmount(arg.book3);
-    this.book4 = normalizeAmount(arg.book4);
-    this.book5 = normalizeAmount(arg.book5);
+    if (argArray === undefined) {
+      this.book1 = normalizeAmount(arg.book1);
+      this.book2 = normalizeAmount(arg.book2);
+      this.book3 = normalizeAmount(arg.book3);
+      this.book4 = normalizeAmount(arg.book4);
+      this.book5 = normalizeAmount(arg.book5);
+    } else {
+      for (const bookN of argArray) {
+        this[bookN]++;
+      }
+    }
   }
 
   /**
